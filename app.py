6324e6f1451e226c611ad35eee91db88a715323c
mario <malopez@vates.com>
2016-12-28 03:00:19 -0300

    add v4l2

--- app.py
+++ app.py
@@ -3,26 +3,28 @@ import json
 import time
 from twython import Twython
 from twython import TwythonStreamer
-from PyV4L2Camera.camera import Camera
-
+from pyv4l2.frame import Frame
 
 class MyStreamer(TwythonStreamer):
 	def initialize(self, handler):
+		print "initialize stremmer"
 		self.text = None
 		self.error = None
 		self.last_update = 0
 		self.handler = handler
+		self.config = handler.config
 
-    def on_success(self, data):
-    	diff = int(time.time()) - self.last_update
-        if 'text' in data and diff > self.config["interval"]:
-        	print "DATA", data
-            text = data['text'].encode('utf-8')
-            data = text.lower().split()
-    		self.handler.dispatcher(data)
+	def on_success(self, data):
+		print "ON SUCCESS"
+		diff = int(time.time()) - self.last_update
+		if 'text' in data and diff > self.config["interval"]:
+			print "DATA", data
+			text = data['text'].encode('utf-8')
+			data = text.lower().split()
+			self.handler.dispatcher(data)
 
-    def on_error(self, status_code, data):
-        self.error = str(status_code)
+	def on_error(self, status_code, data):
+		self.error = str(status_code)
 
 
 
@@ -33,14 +35,14 @@ class Cambot(object):
 		self.stream = None
 		self.twitter = Twython(self.config["app_key"],
 							   self.config["app_secret"],
-                    		   self.config["oauth_token"], 
-                    		   self.config["oauth_secret"])
+							   self.config["oauth_token"], 
+							   self.config["oauth_secret"])
 
 	def start_streamming(self):
 		self.stream = MyStreamer(self.config["app_key"],
 								 self.config["app_secret"],
-                    			 self.config["oauth_token"], 
-                    			 self.config["oauth_secret"])
+								 self.config["oauth_token"], 
+								 self.config["oauth_secret"])
 		self.stream.initialize(self)
 		self.stream.statuses.filter(track=self.config["filter"])	
 
@@ -58,6 +60,17 @@ class Cambot(object):
 				self.get_picture(idx)
 
 	def get_picture(self, idx):
-		camera = Camera('/dev/video%d' % idx)
-		frame = camera.get_frame()
+		frame = Frame('/dev/video%d' % idx)
+		frame_data = frame.get_frame()
+		print frame_data
 
+
+def main():
+	cambot= Cambot()
+	cambot.start_streamming
+	while True:
+		time.sleep(5)
+
+
+if __name__ == '__main__':
+	main()
